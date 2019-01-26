8f65aa33bae565d7caddc48050ce84aeabc45759
Hideki J <aledinai@gmail.com>
2019-01-26 15:39:29 -0200

    Updates

---
+++ precache-manifest.6f5c2421bc88ec9462634ac4c77813de.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4604bc233669f77459c545a01a3f3cc",
+    "url": "/Udacity-FrontEnd-P04-MyReads/static/media/bmark.b4604bc2.svg"
+  },
+  {
+    "revision": "1e294aaecc5280c38010",
+    "url": "/Udacity-FrontEnd-P04-MyReads/static/css/main.7189c593.chunk.css"
+  },
+  {
+    "revision": "448d8998138f3559387d",
+    "url": "/Udacity-FrontEnd-P04-MyReads/static/js/1.448d8998.chunk.js"
+  },
+  {
+    "revision": "c60ff4136bb6f1214051",
+    "url": "/Udacity-FrontEnd-P04-MyReads/static/js/runtime~main.c60ff413.js"
+  },
+  {
+    "revision": "9b4872537159f92a93b7deda6cefb721",
+    "url": "/Udacity-FrontEnd-P04-MyReads/static/media/char.9b487253.svg"
+  },
+  {
+    "revision": "1e294aaecc5280c38010",
+    "url": "/Udacity-FrontEnd-P04-MyReads/static/js/main.1e294aae.chunk.js"
+  },
+  {
+    "revision": "0f57ef90526f17982d953320a6475954",
+    "url": "/Udacity-FrontEnd-P04-MyReads/static/media/add.0f57ef90.svg"
+  },
+  {
+    "revision": "f8765f1c36e7804293bb9070aa6e451d",
+    "url": "/Udacity-FrontEnd-P04-MyReads/static/media/arrow-back.f8765f1c.svg"
+  },
+  {
+    "revision": "f8337b3092e2013fe1ef955e34efff97",
+    "url": "/Udacity-FrontEnd-P04-MyReads/static/media/arrow-changer.f8337b30.svg"
+  },
+  {
+    "revision": "8a07bca3ded189836f6333b5b1efcfa5",
+    "url": "/Udacity-FrontEnd-P04-MyReads/static/media/mainBg.8a07bca3.svg"
+  },
+  {
+    "revision": "560271e4712d71173f03b2dcdf1229ef",
+    "url": "/Udacity-FrontEnd-P04-MyReads/index.html"
+  }
+];
